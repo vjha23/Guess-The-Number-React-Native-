cc97a9bd1af281683f9985887bdfbc8df88b6eb4
Vijay Jha <vjha2323@gmail.com>
2021-02-26 12:22:27 +0530

    created Screen components

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native'
+import Card from '../components/Card'
+
+function StartGameScreen(props) {
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.title}>Start a New Game</Text>
+            <Card style={styles.inputContainer}>
+                <Text>Select A Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title='Reset' onPress={() => { }} />
+                    <Button title='Confirm' onPress={() => { }} />
+                </View>
+            </Card>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    }
+})
+
+export default StartGameScreen
